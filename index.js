7e1102d1e0a7bc293532f2981620e66c5e9ed8ee
Braydon Gines <ginesbraydon@gmail.com>
2019-12-25 11:47:41 -0700

    Modified vscode settings for linter.

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-//Require Discord.js for content
-//Require config.json for settings and configuration, gives security
+// Require Discord.js for content
+// Require config.json for settings and configuration, gives security
+const fs = require('fs');
 const Discord = require('discord.js');
-const {prefix, token} = require('./config.json');
+const { prefix, token } = require('./config.json');
 console.log('Required Files Loaded.');
 
-//Connect bot to a client.
+// Connect bot to a client.
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 console.log('Connected to new discord client.');
 
-//Say ready in console when bot is online and connected.
+// Use the Given filre service to search for a file called commands.
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+// Say ready in console when bot is online and connected.
 client.once('ready', () => {
-  console.log('Ready!');
+	console.log('Ready!');
 });
 
-//Say pong to notify bot is online
+// Say pong to notify bot is online
 client.on('message', message => {
-  if (message.content.startsWith(`${prefix}ping`)){
-    message.channel.send('Pong.');
-  }
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	}
 });
 
-//require token to send all requests.
+// require token to send all requests.
 client.login(token);
