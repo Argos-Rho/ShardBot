b6f964386d6abd3c5770fd3644f52b905caabd2e
Braydon Gines <ginesbraydon@gmail.com>
2019-12-25 02:08:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+//Require Discord.js for content
+//Require config.json for settings and configuration, gives security
 const Discord = require('discord.js');
-console.log('Require discord.js');
+const {prefix, token} = require('./config.json');
+//Connect bot to a client.
 const client = new Discord.Client();
-console.log('Connect to discord client');
 
-client.once('ready', () =>{
+//Say ready in console when bot is online and connected.
+client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.login('');
-console.log('Token Accepted');
+//Repeat any message the bot sees to the console.
+client.on('message', message => {
+  console.log(message.content);
+})
+
+//require token to send all requests.
+client.login(token);
