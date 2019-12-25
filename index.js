ded48c1f09159fa9392074f061037082fe14f026
Braydon Gines <ginesbraydon@gmail.com>
2019-12-25 02:20:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
 //Require config.json for settings and configuration, gives security
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
+console.log('Required Files Loaded.');
+
 //Connect bot to a client.
 const client = new Discord.Client();
+console.log('Connected to new discord client.');
 
 //Say ready in console when bot is online and connected.
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-//Repeat any message the bot sees to the console.
+//Say pong to notify bot is online
 client.on('message', message => {
-  console.log(message.content);
-})
+  if (message.content.startsWith(`${prefix}ping`)){
+    message.channel.send('Pong.');
+  }
+});
 
 //require token to send all requests.
 client.login(token);
