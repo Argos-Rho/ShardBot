8eec87c2cc6aeeea0348c7ed4c74e99a0c2688eb
Braydon Gines <ginesbraydon@gmail.com>
2019-12-25 01:55:34 -0700

    Commit index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+console.log('Require discord.js');
+const client = new Discord.Client();
+console.log('Connect to discord client');
+
+client.once('ready', () =>{
+  console.log('Ready!');
+});
+
+client.login('token-goes-here');
+console.log('Token Accepted');
