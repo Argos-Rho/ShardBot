ef491b07872ae51041dfe5b7c44fc72c1cf96a82
Braydon Gines <ginesbraydon@gmail.com>
2019-12-25 21:57:13 -0700

    commit on master

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-// Required files to start the program are delcared here.
 const fs = require('fs');
 const Discord = require('discord.js');
-// eslint-disable-next-line no-unused-vars
 const { prefix, token } = require('./config.json');
-console.log('Dependencies Loaded.');
 
-// Connect bot to discord
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-console.log('Connecting to discord.');
 
-// Use the file service to search for a folder called commands.
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Allows any correctly formated and delcared commands to be loaded and requested anytime.
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-// Say ready in console when bot is online and connected.
+const cooldowns = new Discord.Collection();
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// put something here later.
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return;
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	const command = client.command.get(commandName);
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
 		command.execute(message, args);
@@ -44,5 +72,4 @@ client.on('message', message => {
 	}
 });
 
-// require token to send all requests.
 client.login(token);
